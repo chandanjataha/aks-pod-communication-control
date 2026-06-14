b02fcbe2ee572d3273d6bb17493d59eff7eebffb
chandanjataha <chandanjataha@gmail.com>
2026-06-14 23:44:17 +0530

    added all code

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.77.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+  subscription_id = "9441bbd5-0154-42cc-a0bb-d6c94e0f0b3b"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "rg-develop"
+    location            = "East US 2"
+  
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-dev-robo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdev"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = "standard_d2ads_v7"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
+}
